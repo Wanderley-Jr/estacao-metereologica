18a9553d98a8c5ce94644cf34696495a124f4fd2
Red-acted <juninhoplay728@gmail.com>
2023-10-02 02:03:09 -0300

    -Monitoração de temperatura e humidade pelo celular -Ligar/desligar led com o celular

--- estacao_metereologica/estacao_metereologica.ino
+++ estacao_metereologica/estacao_metereologica.ino
@@ -1,40 +1,136 @@
-#include <Adafruit_Sensor.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
 #include <DHT.h>
-#include <DHT_U.h>
 
-#define DHTPIN 12
+// Inicialização do DHT
+#define DHTPIN 33
 #define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
-//DHT dht(DHTPIN, DHTTYPE);
+float temperature = 0;
+float humidity = 0;
+
+// Pino do led
+#define LEDPIN 32
+
+// Inicialização do cliente wifi e MQTT
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// Informações sobre o wi-fi
+const char* ssid = "FBI_2G";
+const char* password = "taw65556";
+
+// Informações do broker MQTT
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+
   Serial.begin(115200);
-  //dht.begin();
+  dht.begin();
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
-// Mostra no monitor serial a temperatura e humidade do ar.
-void logAirConditions() {
-  long humidity = dht.readHumidity();
-  long temperature = dht.readTemperature();
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando à rede ");
+  Serial.println(ssid);
 
-  if (humidity != 2147483647 && temperature != 2147483647) {
-    Serial.print("Humidade: ");
-    Serial.print(humidity);
+  WiFi.begin(ssid, password);
 
-    Serial.print("%\tTemperatura: ");
-    Serial.print(temperature);
-    Serial.println("°C");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  else {
-    Serial.println("Erro na leitura do sensor!");
+
+  Serial.println("");
+  Serial.println("Conectado à rede wi-fi!");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Realizando conexão MQTT...");
+    // Attempt to connect
+    if (client.connect("esp32-" + millis())) {
+      Serial.println("conectado");
+
+      // Subscribe
+      client.subscribe("esp32/led");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
 }
 
 void loop() {
-  // logAirConditions();
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-  Serial.println(analogRead(12));
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Temperature in Celsius
+    temperature = dht.readTemperature();   
+    
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temperature, 1, 2, tempString);
+    Serial.print("Temperature: ");
+    Serial.println(tempString);
+    client.publish("esp32/temperature", tempString);
 
-  delay(1000);
+    humidity = dht.readHumidity();
+    
+    // Convert the value to a char array
+    char humString[8];
+    dtostrf(humidity, 1, 2, humString);
+    Serial.print("Humidity: ");
+    Serial.println(humString);
+    client.publish("esp32/humidity", humString);
+  }
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Mensagem recebida no tópico: ");
+  Serial.print(topic);
+  Serial.print(". Mensagem: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/led") {
+    if(messageTemp == "on"){
+      Serial.println("Ligando led!");
+      digitalWrite(LEDPIN, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("Desligando led!");
+      digitalWrite(LEDPIN, LOW);
+    }
+  }
 }
