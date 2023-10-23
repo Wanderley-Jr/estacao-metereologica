4a561f809c64816eac6bfcec3bef74f4af5e26d1
Wanderley-Jr <146687967+Wanderley-Jr@users.noreply.github.com>
2023-10-23 13:44:16 -0300

    Update upload_medida.php

--- servidor/upload_medida.php
+++ servidor/upload_medida.php
@@ -1,16 +1,17 @@
-<!-- Recebe as medidas via GET e as salva na database -->
-
 <?php
-    $temperatura = $_GET["temperatura"];
-    $umidade = $_GET["umidade"];
+    # Recebe os dados do esp32 e realiza o upload ao banco de dados.
+    $data = json_decode(file_get_contents("php://input"), true);
 
-    if (isset($temperatura) && isset($umidade)) {
-        $db = new mysqli("localhost", "root");
-        $db->query("INSERT INTO estacao_metereologica.medidas(temperatura, umidade) VALUES ($temperatura, $umidade)");
-        http_response_code(200); // Ou 201
-    }
-
-    else {
+    if (!isset($data["temperatura"]) || !isset($data["umidade"])) {
         http_response_code(400); // Malformed syntax
+        echo file_get_contents("php://input");
+        return;
     }
+
+    $temperatura = $data["temperatura"];
+    $umidade = $data["umidade"];
+
+    $db = new mysqli("localhost", "root", "admin");
+    $db->query("INSERT INTO estacao_metereologica.medidas(temperatura, umidade) VALUES ($temperatura, $umidade)");
+    http_response_code(200); // Ou 201
 ?>
