6c75eb1f37754c62435d589def022d1ec26ff58b
Wanderley-Jr <146687967+Wanderley-Jr@users.noreply.github.com>
2023-10-23 13:43:06 -0300

    Update estacao_metereologica.ino

--- estacao_metereologica/estacao_metereologica.ino
+++ estacao_metereologica/estacao_metereologica.ino
@@ -1,45 +1,30 @@
 #include <WiFi.h>
-#include <PubSubClient.h>
-#include <Wire.h>
+//#include <Wire.h>
 #include <DHT.h>
+#include <HTTPClient.h>
 
 // Inicialização do DHT
 #define DHTPIN 33
 #define DHTTYPE DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
 
 float temperature = 0;
 float humidity = 0;
 
-// Pino do led
-#define LEDPIN 32
-
-// Inicialização do cliente wifi e MQTT
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-// Informações sobre o wi-fi
+// Dados sobre o Wi-Fi
 const char* ssid = "FBI_2G";
-const char* password = "taw65556";
+const char* password = "123";
 
-// Informações do broker MQTT
-const char* mqtt_server = "broker.hivemq.com";
-const int mqtt_port = 1883;
+// Dados sobre o servidor PHP
+String server = "http://192.168.1.2/estacao_metereologica/upload_medida.php";
 
 void setup() {
-  pinMode(LEDPIN, OUTPUT);
-
   Serial.begin(115200);
   dht.begin();
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
 }
 
-void setup_wifi() {
+void connect_wifi() {
   delay(10);
   Serial.println();
   Serial.print("Conectando à rede ");
@@ -56,81 +41,54 @@ void setup_wifi() {
   Serial.println("Conectado à rede wi-fi!");
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Realizando conexão MQTT...");
-    // Attempt to connect
-    if (client.connect("esp32-" + millis())) {
-      Serial.println("conectado");
-
-      // Subscribe
-      client.subscribe("esp32/led");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+long lastMsg = 0;
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
+  // A cada 5 segundos, realiza uma request POST para o servidor, enviando os dados medidos pelo sensor DHT.
   long now = millis();
   if (now - lastMsg > 5000) {
     lastMsg = now;
-    
-    // Temperature in Celsius
-    temperature = dht.readTemperature();   
-    
-    // Convert the value to a char array
-    char tempString[8];
-    dtostrf(temperature, 1, 2, tempString);
-    Serial.print("Temperature: ");
-    Serial.println(tempString);
-    client.publish("esp32/temperature", tempString);
 
-    humidity = dht.readHumidity();
+    // Manter conexão wi-fi ativa.
+    if (WiFi.status() != WL_CONNECTED) {
+      connect_wifi();
+    }
     
-    // Convert the value to a char array
-    char humString[8];
-    dtostrf(humidity, 1, 2, humString);
-    Serial.print("Humidity: ");
-    Serial.println(humString);
-    client.publish("esp32/humidity", humString);
+    temperature = dht.readTemperature();   
+    humidity = dht.readHumidity();
+
+    enviarMedidas(temperature, humidity);
   }
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Mensagem recebida no tópico: ");
-  Serial.print(topic);
-  Serial.print(". Mensagem: ");
-  String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
+void enviarMedidas(float temperature, float humidity) {
+  HTTPClient http;
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
+  http.begin(server.c_str());
+  http.addHeader("Content-Type", "application/json");
 
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
-  if (String(topic) == "esp32/led") {
-    if(messageTemp == "on"){
-      Serial.println("Ligando led!");
-      digitalWrite(LEDPIN, HIGH);
-    }
-    else if(messageTemp == "off"){
-      Serial.println("Desligando led!");
-      digitalWrite(LEDPIN, LOW);
-    }
+  String body = "{ ";
+  body += "\"temperatura\": ";
+  body += temperature;
+  body += ", ";
+  body += "\"umidade\": ";
+  body += humidity;
+  body += " }";
+
+  int httpResponseCode = http.POST(body.c_str());
+
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    Serial.println("Payload: ");
+    String payload = http.getString();
+    Serial.println(payload);
   }
+  else {
+    Serial.print("Ocorreu um erro, HTTP Response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
 }
