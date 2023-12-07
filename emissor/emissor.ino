83be35fbf2f2d035e0d0b3b85910cea408db5819
Wanderley-Jr <wanderleyjunior284@gmail.com>
2023-12-07 13:43:37 -0300

    Now sends data through 16-byte packets; Added AES-128 encryption

--- emissor/emissor.ino
+++ emissor/emissor.ino
@@ -1,10 +1,10 @@
 #include <heltec.h>
-#include <ArduinoJson.h>
-
 #include <Adafruit_BMP280.h>
 #include <DHT.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Crypto.h>
+#include <AES.h>
 
 #include "CapacitiveSensor.cpp"
 
@@ -28,6 +28,12 @@ OneWire wire(SOIL_TEMPERATURE_PIN);
 DallasTemperature soilTemperatureSensor(&wire); // Soil temperature
 Adafruit_BMP280 barometer; // Barometer
 
+/* 
+ * AES setup
+ */
+AES128 aes128;
+byte aesKey[16] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+
 void setup() {
   // Serial monitor setup
   Serial.begin(115200);
@@ -39,6 +45,9 @@ void setup() {
     Serial.println("Fail on LoRa startup!");
     delay(1000);
   }
+
+  // AES128 setup
+  aes128.setKey(aesKey, 16);
 
   // Sensor setup
   dht.begin();
@@ -61,35 +70,60 @@ void setup() {
   Serial.println("Setup finished!");
 }
 
-String readData() {
+// 16 bits
+struct Packet {
+  unsigned int id;
+  float value;
+  char name[5];
+};
+
+void sendPacket(int id, char name[5], float value) {
+  // Create structure
+  Packet packet;
+  for (int i = 0; i < 5; i++) {
+    packet.name[i] = name[i];
+  }
+  packet.id = id;
+  packet.value = value;
+
+  // Encrypt
+  byte encrypted[16];
+  aes128.encryptBlock(encrypted, (byte*) &packet);
+
+  // Send encrypted packet through LoRa
+  LoRa.beginPacket();
+  LoRa.write(encrypted, sizeof(encrypted));
+  LoRa.endPacket();
+
+  Serial.print("Sent packet: ");
+  for (int i = 0; i < 16; i++) {
+    Serial.print(encrypted[i]);
+  }
+  Serial.println();
+}
+
+int packetId = 0;
+
+void sendData() {
+  // Certifies that each group of packets is a multiple of 7 (because there are 7 sensors)
+  if (packetId % 7) {
+    packetId += 7 - (packetId%7);
+  }
+
   soilTemperatureSensor.requestTemperatures();
 
-  StaticJsonDocument<1024> json;
-
-  json["temperature"] = dht.readTemperature();
-  json["humidity"] = dht.readHumidity();
-  json["soil_temperature"] = soilTemperatureSensor.getTempCByIndex(0);
-  json["soil_humidity"] = soilHumiditySensor.read();
-  json["rain"] = rainSensor.read();
-  json["airPressure"] = barometer.readPressure();
-  json["luminosity"] = lightSensor.read();
-
-  String result;
-  serializeJson(json, result);
-  return result;
+  sendPacket(packetId++, "aTmp", dht.readTemperature()); // Air temperature
+  sendPacket(packetId++, "aHmd", dht.readHumidity()); // Air humidity
+  sendPacket(packetId++, "sTmp", soilTemperatureSensor.getTempCByIndex(0)); // Soil temperature
+  sendPacket(packetId++, "sHmd", soilHumiditySensor.read());
+  sendPacket(packetId++, "rai", rainSensor.read()); // Rain sensor
+  sendPacket(packetId++, "airP", barometer.readPressure()); // Air pressure
+  sendPacket(packetId++, "lumi", lightSensor.read()); // Luminosity
 }
 
 void loop() {
   // Send data every 5 seconds
   Serial.println("Sending data.....");
-
-  String json = readData();
-  LoRa.beginPacket();
-  LoRa.print(json);
-  LoRa.endPacket();
-
-  Serial.print("Sent data: ");
-  Serial.println(json);
-
-  delay(15000);
+  sendData();
+  delay(1000);
 }
--- receptor/receptor.ino
+++ receptor/receptor.ino
@@ -1,5 +1,21 @@
 #include <heltec.h>
 #include <SPI.h>
+#include <Crypto.h>
+#include <AES.h>
+#include <ArduinoJson.h>
+
+// 16 bits
+struct Packet {
+  unsigned int id;
+  float value;
+  char name[5];
+};
+
+/* 
+ * AES setup
+ */
+AES128 aes128;
+byte aesKey[16] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
 
 void setup() {
 	// Serial setup
@@ -10,6 +26,9 @@ void setup() {
 	Heltec.begin();
 	Heltec.display->displayOn();
 
+	// AES128
+	aes128.setKey(aesKey, 16);
+
 	// LoRa setup
 	LoRa.setPins(18, 14, 26);
 	while (!LoRa.begin(433E6, true)) {
@@ -17,27 +36,47 @@ void setup() {
 		delay(1000);
 	}
 	Serial.println("Setup finished!!!");
-	LoRa.onReceive(onReceive);
 	LoRa.receive();
 }
 
-int receivedPackets = 0;
-String packet;
-
-void onReceive(int packetSize) {
-	receivedPackets++;
-	Serial.println("Packet recebida!");
-	packet = LoRa.readString();
-}
+StaticJsonDocument<1024> data;
 
 void loop() {
-	Serial.print("Mostrando na tela: ");
-	Serial.println(packet);
+	Packet packet;
 
-	Heltec.display->clear();
-	Heltec.display->drawString(0, 0, String("Packets recebidas: ") + receivedPackets);
-	Heltec.display->drawString(0, 10, String("Rssi: ") + LoRa.packetRssi());
-	Heltec.display->drawString(0, 30, packet);
-	Heltec.display->display();
-	delay(10000);
+	do {
+		// Wait for packet
+		while (!LoRa.parsePacket()); 
+
+		// Read received packet
+		byte encrypted[16];
+		LoRa.readBytes(encrypted, sizeof(encrypted));
+		aes128.decryptBlock((byte*) &packet, encrypted);
+
+		// Write data from packet to json
+		data[packet.name] = packet.value;
+	}
+	while (packet.id % 7 != 6);
+
+	// If there aren't 7 elements, we failed to receive at least one sensor reading.
+	if (data.size() != 7) {
+		Serial.println("Failed to gather sensor data!");
+		return;
+	}
+
+	String result;
+	serializeJson(data, result);
+	Serial.print("Result: ");
+	Serial.println(result);
+	data.clear();
+
+	// Serial.print("Mostrando na tela: ");
+	// Serial.println(packet);
+
+	// Heltec.display->clear();
+	// Heltec.display->drawString(0, 0, String("Packets recebidas: ") + receivedPackets);
+	// Heltec.display->drawString(0, 10, String("Rssi: ") + LoRa.packetRssi());
+	// Heltec.display->drawString(0, 30, packet);
+	// Heltec.display->display();
+	// delay(100);
 }
